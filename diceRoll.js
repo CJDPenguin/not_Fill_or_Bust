7b47e31a5dacf0a339a9424bf771a0f87f5efd8e
Chris Davis <christopher.j.davis03@gmail.com>
2022-09-11 12:47:28 -0500

    making some progress

--- diceRoll.js
+++ diceRoll.js
@@ -1,5 +1,5 @@
 function diceRoll() {
     let roll = Math.floor((Math.random() * 6) + 1);
     console.log(roll);
-    document.querySelector('#rollValue').innerHTML = roll.toString();
+    return roll;
 }
