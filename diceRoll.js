ec5fa7192a1c4624773e0e817f1675924020ccf8
Chris Davis <christopher.j.davis03@gmail.com>
2022-09-12 15:11:00 -0500

    DOM list creation

--- diceRoll.js
+++ diceRoll.js
@@ -1,5 +1,12 @@
-function diceRoll() {
+function roll() {
     let roll = Math.floor((Math.random() * 6) + 1);
-    console.log(roll);
     return roll;
+}
+
+function diceRoll() {
+    let rolls = document.getElementsByClassName('roll');
+    let kept = Array.from(document.getElementsByClassName('keep'));
+
+    console.log(kept.length);
+    console.log(rolls.length);
 }
