daf11756b501680cf5e5a7bd77ce2d30e99571ca
Chris Davis <christopher.j.davis03@gmail.com>
2022-09-28 13:28:46 -0500

    card shuffle start

--- cardDeck.js
+++ cardDeck.js
@@ -37,3 +37,14 @@ class Deck {
         }
     }
 }
+
+function shuffleDeck(deck){
+    let currentIndex = deck.length, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [deck[currentIndex], deck[randomIndex]] = [deck[randomIndex], deck[currentIndex]];
+    }
+    return deck;
+}
